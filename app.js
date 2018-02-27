408c907334d6cc2cd90c17737ec9549c066a4c82
Tucker McKinney <noemata83@gmail.com>
2018-02-26 20:59:16 -0500

    Bugfix; scraper extracts ingredient data from sites using some alternate prop names

--- app.js
+++ app.js
@@ -63,24 +63,24 @@ const getItemProps = (document, recipe) => {
         rawData.forEach(elem => { dataArray.push(elem.textContent.replace(/\s{3,}/g, ' ').trim()); });
         if (dataArray.length === 0) {
             console.log(key + " is length 0");
-            const alternateNameData = getByAlternateNames(document, key);
-            console.log(getByAlternateNames(document, key));
-            console.log(alternateNameData);
+            dataArray = getByAlternateNames(document, key);
         }
-        recipe[key] = dataArray;
+        console.log(dataArray);
+        recipe[key] = dataArray.filter(el => el !== '');
     }
     return recipe;
 }
 
 const getByAlternateNames = (document, key) => {
-    alternateNames[key].forEach(name => {
+//    alternateNames[key].forEach(name => {
+    for (let i = 0; i < alternateNames[key].length; i++) {
         let dataArray = [];
-        let rawData = document.querySelectorAll(`[itemprop="${name}"]`);
+        let rawData = document.querySelectorAll(`[itemprop="${alternateNames[key][i]}"]`);
         rawData.forEach(elem => { dataArray.push((elem.textContent).replace(/\s{3,}/g, ' ').trim()); });
         if (dataArray.length > 0) {
             console.log("I'm returning: ", dataArray);
             return dataArray;
         }
-    });
+    }
     return [];
 }
